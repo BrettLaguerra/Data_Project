fedb7f7be6f8b46f1abdd877038b346b279cf90f
Brett Laguerra <b.laguerra@yahoo.com>
2018-03-21 09:19:12 -0400

    working on avg rent def

--- connect.py
+++ connect.py
@@ -2,17 +2,3 @@ import sqlite3
 
 conn = sqlite3.connect('rent.db')
 cur = conn.cursor()
-
-def create_table():
-    cur.execute('CREATE TABLE IF NOT EXISTS stuffToPlot(County TEXT, State TEXT, Median_Cost REAL)')
-
-
-def data_entry():
-    cur.execute('INSERT INTO stuffToPlot VALUES("Jefferson", "KY", 1000)')
-    conn.commit()
-    cur.close()
-    conn.close()
-
-
-create_table()
-data_entry()
--- raw_data.py
+++ raw_data.py
@@ -1,20 +1,38 @@
 import csv
+import sqlite3
 import numpy
 
-def open_with_csv(filename, d=','):
+
+def open_with_csv(filename, cursor, d=','):
     data = []
+    #open sqlite
     with open(filename) as tsvin:
         rent_reader = csv.reader(tsvin, delimiter=d)
+        cv = 0
         for line in rent_reader:
-            data.append(line)
-    return data
+            if cv > 0:
+                try:
+                    insert_string = "INSERT INTO rent_table (State, County, City, Mean_Cost, Median_Cost, Standard_Deviation) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}');".format(line[2], line[4], line[5], line[15], line[16], line[17])
+                    cursor.execute(insert_string)
+                else:
+                    print(insert_string)
+            cv += 1
+        return data
 
 
-FIELDNAMES = ['id', 'State_Code', 'State_Name', 'State_ab', 'County', 'City', 'Place', 'Type', 'Primary', 'Zip_Code', 'Area_Code', 'ALand', 'AWater', 'Lat', 'Lon', 'Mean', 'Median', 'Stdev', 'Samples']
+def create_table(cur):
+    cur.execute("DROP TABLE rent_table;")
+    cur.execute("CREATE TABLE IF NOT EXISTS rent_table (State TEXT, County TEXT, City TEXT, Mean_Cost REAL, Median_Cost REAL, Standard_Deviation REAL)")
+#FIELDNAMES = ['id', 'State_Code', 'State_Name', 'State_ab', 'County', 'City', 'Place', 'Type', 'Primary', 'Zip_Code', 'Area_Code', 'ALand', 'AWater', 'Lat', 'Lon', 'Mean', 'Median', 'Stdev', 'Samples']
 
-data_from_csv = open_with_csv('rent_data.csv')
+
+def average_rent(cur):
+    cur.execute("SELECT ROUND(AVG(Mean_Cost)) FROM rent_table WHERE State = 'Alabama'")
 
 
-def load_data(filename, d=','):
-    my_csv = numpy.genfromtxt(filename, delimiter=d, skip_header=1, invalid_raise=False, names=FIELDNAMES)
-    return my_csv
+conn = sqlite3.connect('rent.db')
+cur = conn.cursor()
+#step 1: create_table(cur)
+open_with_csv('rent_data.csv', cur)
+conn.commit()
+cur.close()
